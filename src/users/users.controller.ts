1babcf84577e8c8b831bea5f93bc41e20fab6ee7
script-lab <transmit.work@gmail.com>
2022-01-30 15:54:20 +0900

    [add] ユーザー作成の処理を追加
    controller と service を作成し、ユーザー作成の処理を追加した。

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: createUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+
+  create(newUser: User) {
+    this.users.push(newUser);
+  }
+}
